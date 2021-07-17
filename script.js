c4923dc315c06dd092781af97a0b0384f07428f8
AliRaza <32015381+AliTechGeek52@users.noreply.github.com>
2021-07-17 23:58:28 +0500

    files updated

--- script.js
+++ script.js
@@ -11,3 +11,36 @@ closeMenu.addEventListener('click', ()=>{
 });
 
 
+
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.slide-in');
+
+const appearoptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -250px 0px"
+};
+const appearonscroll = new IntersectionObserver(function(enteries, appearonscroll) {
+    enteries.forEach(entry => {
+        if(!entry.isIntersecting){
+            return;
+        } else {
+            entry.target.classList.add('appear');
+            appearonscroll.unobserve(entry.target);
+        }
+    });
+}, appearoptions);
+
+faders.forEach(fader => {
+    appearonscroll.observe(fader);
+});
+
+
+
+sliders.forEach(slider => {
+    appearonscroll.observe(slider);
+});
+
+
+
+
+
